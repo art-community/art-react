71ebb516614eb05034434aab556c4c3162d73fb0
anton.bashirov <anton.sh.local@gmail.com>
2020-12-14 04:11:12 +0300

    ART React

--- index.ts
+++ index.ts
@@ -1,18 +1,15 @@
 import * as ReactDOM from "react-dom";
 import {button} from "./dsl/managed/ManagedButton";
 import {MAIN_COMPONENT} from "./constants/Constants";
-import {horizontalGrid, verticalGrid} from "./dsl/managed/ManagedGrid";
+import {verticalGrid} from "./dsl/managed/ManagedGrid";
 import {label} from "./dsl/managed/ManagedLabel";
 import {information} from "./dsl/managed/ManagedDialog";
 import {text} from "./dsl/managed/ManagedTextField";
 import {checkBoxPanel} from "./dsl/managed/ManagedPanel";
 import {tooltip} from "./dsl/simple/SimpleTooltip";
 
-window.addEventListener("beforeunload", event => {
-    event.preventDefault();
-});
-
-let informationDialog = information({information: "Инфо", label: "Информация", buttonLabel: "ОК"});
+const informationDialog = information({information: "Инфо", label: "Информация", buttonLabel: "ОК"});
+const tooltipText = label("Test");
 const widget = verticalGrid({spacing: 2})
     .pushWidget(label({text: "Это кнопка:"}))
     .pushWidget(button({
@@ -21,10 +18,8 @@ const widget = verticalGrid({spacing: 2})
         }).onClick(() => informationDialog.useVisible(visible => visible.set(!visible.value)))
     )
     .pushWidget(checkBoxPanel(tooltip({placement: "top-start"})
-        .widget(text({label: "MyText", fullWidth: true}).onTextChanged(console.log))
-        .title(label("Test")), {
-        label: "Test"
-    })
-    .add(informationDialog))
-;
+        .widget(text({label: "MyText", fullWidth: true}).onTextChanged(console.log).onTextChanged(tooltipText.setText))
+        .title(tooltipText), {label: "Test"})
+    )
+    .add(informationDialog);
 ReactDOM.render(widget.render(), document.getElementById(MAIN_COMPONENT));
